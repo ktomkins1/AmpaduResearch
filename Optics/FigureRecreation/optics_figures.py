dc4554d2b8643c3825e2e54eb32a267adf34312e
Kevin Tomkins <ktomkins1@gmail.com>
2021-01-01 21:32:24 -0500

    initial primative quantities of the system

---
+++ Optics/FigureRecreation/optics_figures.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib
+
+'''
+    the following quantities describe the system:
+    E_i         the E-field of the i-th block*
+    dtX         the time derivative of the X (dtE_i would be of E_i)
+    N_i         the carrier density of the i-th block*
+    theta       the phase difference between blocks in a 2-block system
+    alpha       the line-width enhancement factor
+    LAMBDA      the excess pumping rate*
+    DELTA       the cavity optical detuning
+    T           the ratio of carrier to photon lifetimes
+    t           the time of the system, normalized to photon lifetime
+    tau_p       the photon lifetime
+'''
+
+if __name__ == '__main__':
+    
+    pass
+    
