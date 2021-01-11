2c83c558fa001532b2647a5689e6c26bd7180d6c
ktomkins1 <tomkinskevin1@gmail.com>
2021-01-11 02:14:11 -0500

    testing updates for the actual plotting of series

--- Optics/FigureRecreation/testing_real.py
+++ Optics/FigureRecreation/testing_real.py
@@ -2,7 +2,7 @@ import sympy as sp
 from sympy import sin,cos
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import ode
+from scipy.integrate import ode, solve_ivp
 
 
 '''
@@ -54,33 +54,60 @@ def int_ode(y0,time,funcs):
         res.append(r.y)
     return res
     
-def main_1():
-    funcs = setup(10**(-3.5), 0, 4, 0.0002, 0.0002, 1000)
+def get_ivals_from_characteristics(E_0, rho, theta_s, alpha, LAMBDA):
+    DELTA = -alpha*LAMBDA*np.sin(theta_s)*(rho**-1 + rho) - LAMBDA*np.cos(theta_s)*(rho**-1 - rho)
+    
+    P_1 = E_0**2 + (1 + 2*E_0**2)*LAMBDA*rho*np.sin(theta_s)
+    R2 = rho**2
+    P_2 = R2*(E_0**2) - (1 + 2*R2*(E_0**2))*LAMBDA*(rho**-1)*np.sin(theta_s)
+    
+    print("dlta:{0}, p1:{1}, p2:{2}".format(DELTA, P_1, P_2))
+    return DELTA, P_1, P_2
+
+def main():
+    #Get the system of equations
+    LAMBDA = 10**(-2.2)
+    alpha = 4
+    theta_s = 2.75
+    rho = 0.24
+    DELTA, P_1, P_2 = get_ivals_from_characteristics(0.5, rho, theta_s, alpha, LAMBDA)
+    funcs = setup(LAMBDA, DELTA, alpha, P_1, P_2, 1000)
     
     #Define time list and  initial value
-    #time = [10**i for i in range(4)] +[0] + [-10**j for j in range(4)]
-    #time = sorted(time)
-    time_step = 0.0001
-    time = np.arange(0,200,time_step)
-    init=[0.5,0.5,0,0.25,0.25]
-
-    #Calculate the result
-    r = int_ode(init,time,funcs)
+    init=[0.5,0.5,0,0,0]
+    
+    r, time = time_and_result_1(init, funcs, 0, 4000)
 
     names = ['E1','E2','theta','N1','N2']
     fig, axes = plt.subplots(5,1)
     fig.suptitle(','.join(map(str,init)))
-    for k in range(len(r[0])):
-        plot_data= [lst[k] for lst in r]
+    for k in range(len(r)):
+        plot_data= r[k]
         ax = axes[k]
         ax.title.set_text(names[k])
         ax.plot(time, plot_data)
     plt.show()
 
-def main_2():
-    pass
+def time_and_result_1(init, funcs, ll, ul):
+    #Define time list and  initial value
+    #time = [10**i for i in range(4)] +[0] + [-10**j for j in range(4)]
+    #time = sorted(time)
+    time_step = 0.0001
+    time = np.arange(ll, ul,time_step)
+    
+    #Calculate the result
+    r = int_ode(init,time,funcs)
+    
+    ret = [[lst[k] for lst in r] for k in range(5)]
+    return ret, time
+
+def time_and_result_2(init, funcs, ll, ul):
+    #Calculate the result
+    res_map = solve_ivp(int_step, (ll, ul), init, args=(funcs,))
+    return res_map['y'], res_map['t']
+    
 
 if __name__ == '__main__':
-    main_1()
+    main()
     
     
