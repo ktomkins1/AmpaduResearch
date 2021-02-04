f72dd3823e7d44b0aa2eccc3974ff8d1300700ba
ktomkins1 <tomkinskevin1@gmail.com>
2021-02-04 00:56:21 -0500

    config files progress

--- Optics/FigureRecreation/initiation.py
+++ Optics/FigureRecreation/initiation.py
@@ -23,8 +23,13 @@ def run_and_compare_sweeps(): #for easy comparison on same plot
 def save_single_sim_traces(config, time_axis, data):
     pass
 
-def save_bifurcation_data(config, axis, data):
-    pass
+def save_bifurcation_data(rdir, config, axis, data):
+    our_obj = {'axis':axis, 'data':data}
+    filepath = os.path.join(rdir,'bf_results_{}'.format(config['desc'])+'.pickle')
+    with open(filepath, 'wb+') as fp:
+        print('Saving data to disk....', end='')
+        pickle.dump(our_obj, fp)
+        print('Done')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -34,6 +39,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     #import config file
+    '''
+      Configs hold all of the parameters of the system.
+      There can be parameters which are varied:
+        one parameter can be given as a tuple: (start, end, number_of_steps)
+        this is what is used as the sweep parameter for a bifurcation diagram
+        
+        one parameter can be given as a [list] of several values
+        one bifurcation diagram or plot will be created for each.  these will
+        then be plotted together (and plots of each alone will also be saved)
+        
+        if no list or range parameters are given, a plot of all of the traces
+        will be created and stored.
+    '''
     config = None
     with open(args.cfilename, 'r') as fp:
         config = json.load(fp)
@@ -44,20 +62,26 @@ if __name__ == '__main__':
         config['desc'] = cc.create_short_desc(config)
 
     #create folder under results
-    results_dirname = 'results/'+config['desc']
+    results_dirname = os.path.join('results', config['desc'])
     os.makedirs(results_dirname, exist_ok=True)
-
+    
     #call simulation(s)
-    vals, bf, fs = sim.eta_sweep(model.setup, eta_start, eta_end, d_eta, P, DLT, T=155, llsim=0, ulsim=6000, llcyc=5500, ulcyc=5999)
+    eta_start = 0.0
+    eta_end = 0.014
+    d_eta = 0.0005
+    vals, bf, fs = sim.eta_sweep(model.setup, eta_start, eta_end, d_eta, 
+                                 config['P'], config['DELTA'], T=config['T'],
+                                 llsim=config['llsim'], ulsim=config['ulsim'],
+                                 llcyc=config['llcyc'], ulcyc=config['ulcyc'])
 
     #call visualization(s)
     #show = vis.plot_n_traces([ys[0], ys[2]], t, ["E1","N1"], "Electric Field for eta={0}, sim@6000,5700:5999".format(eta))
-    show = vis.plot_bif_diag(vals, bf, 'Eta', absv=True)
+    show = vis.plot_bif_diag(vals, bf, 'Eta', config)
     show()
 
     #save data
     save_bifurcation_data(results_dirname, config, vals, bf)
 
     #save configuration
-    with open(results_dirname + '/config_' + config['enc'] + '.json', 'w') as fp:
-        json.dump(config, fp)
+    with open(results_dirname + '/config_' + config['desc'] + '.json', 'w+') as fp:
+        json.dump(config, fp, indent=4)
